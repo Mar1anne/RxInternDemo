70a25153048491b453b277a9c3719a6368dca49d
Mariana <mariana.ristovska@webfactory.mk>
2016-11-22 15:49:20 +0100

    Added base drawer controller

---
+++ InternDemo/PostsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PostsViewController.swift
+//  InternDemo
+//
+//  Created by WF | Mariana on 11/22/16.
+//  Copyright © 2016 WF | Mariana. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
