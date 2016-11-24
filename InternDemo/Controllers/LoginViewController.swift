a7879df7fab9731a874daa3f714028837aec5e1f
Mariana <mariana.ristovska@webfactory.mk>
2016-11-24 14:50:57 +0100

    Added LoginViewController file, added ControllerManager file

---
+++ InternDemo/Controllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  InternDemo
+//
+//  Created by WF | Mariana on 11/24/16.
+//  Copyright © 2016 WF | Mariana. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ InternDemo/Misc/ControllerManager.swift
@@ -0,0 +1,15 @@
+//
+//  ControllerManager.swift
+//  InternDemo
+//
+//  Created by WF | Mariana on 11/24/16.
+//  Copyright © 2016 WF | Mariana. All rights reserved.
+//
+
+import UIKit
+
+class ControllerManager: NSObject {
+
+    // TODO: add user observer, change root when needed
+    
+}
