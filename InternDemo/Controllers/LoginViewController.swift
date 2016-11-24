c79d2b6f288952b42a7b7c60e6d9ed34a1a4c84e
Mariana <mariana.ristovska@webfactory.mk>
2016-11-24 15:42:24 +0100

    LoginViewController UI implementation

--- InternDemo/Controllers/LoginViewController.swift
+++ InternDemo/Controllers/LoginViewController.swift
@@ -8,11 +8,44 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: BaseViewController {
 
+    private let logoLabel = UILabel()
+    private let loginButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    //MARK: - View setup
+    override func setupViews() {
+        super.setupViews()
+        
+        logoLabel.text = "imgur Demo"
+        logoLabel.font = UIFont(name: "", size: 17)
+        
+        loginButton.backgroundColor = .green
+        loginButton.layer.cornerRadius = 20
+        loginButton.clipsToBounds = true
+        loginButton.setTitle("Login", for: .normal)
+        
+        view.addSubview(logoLabel)
+        view.addSubview(loginButton)
+    }
+    
+    override func setupConstraints() {
+        super.setupConstraints()
+        
+        logoLabel.snp.makeConstraints { (make) in
+            make.center.equalTo(view)
+        }
+        
+        loginButton.snp.makeConstraints { (make) in
+            make.top.equalTo(logoLabel.snp.bottom).offset(30)
+            make.width.equalTo(view).multipliedBy(0.6)
+            make.centerX.equalTo(view)
+            make.height.equalTo(40)
+        }
+    }
 }
